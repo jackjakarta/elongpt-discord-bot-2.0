ff82d18a02ac34c5381d10fa2bb4da216fc4f745
jackjakarta <al.termure@gmail.com>
2024-10-31 00:19:47 +0100

    feat: add files upload to ask command

--- bot/ai/chat.py
+++ bot/ai/chat.py
@@ -3,35 +3,62 @@ from openai import OpenAI
 
 from bot.utils.settings import OLLAMA_MODEL, OLLAMA_SERVER, OPENAI_API_KEY, OPENAI_MODEL
 
+from .prompts import DEFAULT_SYSTEM_PROMPT
+
 
 class ChatGPT:
     """ChatGPT Class"""
 
-    def __init__(self, user_name=None, model=OPENAI_MODEL):
+    def __init__(self, system_prompt=DEFAULT_SYSTEM_PROMPT, model=OPENAI_MODEL):
         self.client = OpenAI(api_key=OPENAI_API_KEY)
         self.model = model
         self.prompt = None
         self.completion = None
+        self.files = []
+        self.system_prompt = system_prompt
 
-        self.chats_dir = "CHATS_FOLDER"
-        self.chat_file = f"chat_{user_name}.json"
+        self.messages = [
+            {
+                "role": "system",
+                "content": self.system_prompt,
+            }
+        ]
 
-        self.messages = []
-
-    def ask(self, prompt, max_tokens: int = 400):
+    def ask(self, prompt, files: list | None = None, max_tokens: int = 400):
         self.prompt = prompt
+        self.files = files[:5] if files else []
 
-        if self.prompt:
-            self.messages.append({"role": "user", "content": self.prompt})
+        self.messages = [
+            {
+                "role": "system",
+                "content": self.system_prompt,
+            },
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": self.prompt,
+                    },
+                    *(
+                        [
+                            {
+                                "type": "image_url",
+                                "image_url": {
+                                    "url": f"data:image/jpeg;base64,{image}",
+                                },
+                            }
+                            for image in self.files
+                        ]
+                        if self.files
+                        else []
+                    ),
+                ],
+            },
+        ]
 
         self.completion = self.client.chat.completions.create(
             model=self.model, messages=self.messages, max_tokens=max_tokens
-        )
-        self.messages.append(
-            {
-                "role": "assistant",
-                "content": str(self.completion.choices[0].message.content),
-            }
         )
 
         return self.completion.choices[0].message.content
@@ -41,49 +68,6 @@ class ChatGPT:
         models = [x.id for x in models_list]
 
         return sorted(models)
-
-
-class ImageClassify(ChatGPT):
-    """ImageClassify Class"""
-
-    def __init__(self, prompt="Classify this image."):
-        super().__init__()
-        self.messages = []
-        self.image_url = None
-        self.prompt = prompt
-
-    def classify_image(self, image_url: str):
-        self.image_url = image_url
-        self.messages = []
-
-        msg_dict = {
-            "role": "user",
-            "content": [
-                {
-                    "type": "text",
-                    "text": self.prompt,
-                },
-                {
-                    "type": "image_url",
-                    "image_url": {
-                        "url": self.image_url,
-                    },
-                },
-            ],
-        }
-
-        if self.image_url:
-            self.messages.append(msg_dict)
-
-        try:
-            self.completion = self.client.chat.completions.create(
-                model=self.model, messages=self.messages, max_tokens=650
-            )
-            assistant_response = self.completion.choices[0].message.content
-        except Exception as e:
-            assistant_response = f"Error: {str(e)}"
-
-        return assistant_response
 
 
 class Ollama:
--- bot/utils/__init__.py
+++ bot/utils/__init__.py
@@ -1,6 +1,14 @@
+import base64
+
 from discord import Embed
 
 
 def create_embed(title, description, color=0xFFFFFF):
     """Create a Discord embed."""
     return Embed(title=title, description=description, color=color)
+
+
+def image_to_base64(file_data: str) -> str:
+    base64_image = base64.b64encode(file_data).decode("utf-8")
+
+    return base64_image
