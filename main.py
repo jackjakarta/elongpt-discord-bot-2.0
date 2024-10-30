46a9b5b6793e6d0f5d3eeb98e3cc2224c6c27f17
Alex Termure <124639246+jackjakarta@users.noreply.github.com>
2024-10-30 01:24:04 +0100

    cicd: deploy workflow (#2)

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ DISCORD_TOKEN: Final[str] = config("DISCORD_TOKEN")
 
 @tasks.loop(seconds=5)
 async def change_bot_status():
-    await bot.change_presence(activity=discord.Game(name="with discord.py"))
+    await bot.change_presence(activity=discord.Game(name="/ask"))
 
 
 @bot.event
