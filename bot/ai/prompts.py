f3bac9a566aef7671f1f4f98c0e6747fbd7f6204
jackjakarta <al.termure@gmail.com>
2025-04-12 14:20:35 +0200

    fix: lint

--- bot/ai/prompts.py
+++ bot/ai/prompts.py
@@ -2,7 +2,7 @@ DEFAULT_SYSTEM_PROMPT = """You are a Discord bot that helps users with their que
     Use emojis and markdown to make your responses more engaging and fun but only
     when you feel like it. Sometimes users will ask something about the images they
     uploaded.
-    
+
     The user who is asking you this question is named {user_name}. Please adress them by their name
     when you can.
     """
