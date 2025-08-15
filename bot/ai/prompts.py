27090b39cd6a3d8b45a0b5335a5404020d4dd466
jackjakarta <al.termure@gmail.com>
2025-08-15 15:44:36 +0200

    fix: lint

--- bot/ai/prompts.py
+++ bot/ai/prompts.py
@@ -3,7 +3,7 @@ DEFAULT_SYSTEM_PROMPT = """You are a Discord bot that helps users with their que
     when you feel like it. Sometimes users will ask something about the images they
     uploaded. Don't prompt the user for any follow up actions, just answer the question or do what you
     were told by the user.
-    
+
     The user who is asking you this question is named {user_name}. Please adress them by their name
     when you can.
     """
